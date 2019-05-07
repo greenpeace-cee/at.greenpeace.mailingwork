ddad0fb17312eac79d9f05c6c24124ffb7f40dbf
Patrick Figel <pfigel@greenpeace.org>
2019-05-07 18:17:45 +0200

    Change mailing folder sync to use new folder table

--- CRM/Mailingwork/Processor/Greenpeace/Mailings.php
+++ CRM/Mailingwork/Processor/Greenpeace/Mailings.php
@@ -2,8 +2,10 @@
 
 class CRM_Mailingwork_Processor_Greenpeace_Mailings extends CRM_Mailingwork_Processor_Base {
   public function import() {
+    $this->importFolders();
+    $count = 0;
+    $countExisting = 0;
     foreach ($this->client->api('mailing')->getMailings() as $mailing) {
-      $mailing_id = $mailing->id;
       $mailingDetails = $this->client->api('mailing')->getEmailById($mailing->id);
       $data = [
         'mailingwork_identifier' => $mailing->id,
@@ -11,20 +13,49 @@ class CRM_Mailingwork_Processor_Greenpeace_Mailings extends CRM_Mailingwork_Proc
         'description'            => $mailing->description,
         'sender_name'            => $mailingDetails->senderName,
         'sender_email'           => $mailingDetails->senderEmail,
-        'folder_id'              => $mailingDetails->folderId,
+        'mailingwork_folder_id'  => $this->getFolder($mailingDetails->folderId),
         'sending_date'           => $mailing->sendingTime,
         'status_id'              => $mailing->status,
         'type_id'                => $mailing->type,
       ];
+
       $existing_mailing = civicrm_api3('MailingworkMailing', 'get', [
         'return'                 => ['id'],
         'mailingwork_identifier' => $mailing->id,
       ]);
+
       if ($existing_mailing['count'] > 0) {
         $data['id'] = $existing_mailing['id'];
+        $countExisting++;
       }
+
       civicrm_api3('MailingworkMailing', 'create', $data);
+      $count++;
     }
+
+    return [
+      'count'    => $count,
+      'existing' => $countExisting,
+      'new'      => $count - $countExisting
+    ];
+  }
+
+  private function importFolders() {
+    return civicrm_api3('MailingworkFolder', 'import', [
+      'username' => $this->params['username'],
+      'password' => $this->params['password'],
+    ]);
+  }
+
+  private function getFolder($identifier) {
+    $result = civicrm_api3('MailingworkFolder', 'get', [
+      'return'                 => ['id'],
+      'mailingwork_identifier' => $identifier,
+    ]);
+    if ($result['count'] > 0) {
+      return $result['id'];
+    }
+    return NULL;
   }
 
 }
