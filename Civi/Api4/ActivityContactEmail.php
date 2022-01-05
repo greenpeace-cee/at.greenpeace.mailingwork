a81527af9bfb38bfc9d42275af002d64f16b9b43
Patrick Figel <pfigel@greenpeace.org>
2022-01-05 12:20:03 +0100

    GP-24196 Add API4 entities

---
+++ Civi/Api4/ActivityContactEmail.php
@@ -0,0 +1,13 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * ActivityContactEmail entity.
+ *
+ * Provided by the Mailingwork extension.
+ *
+ * @package Civi\Api4
+ */
+class ActivityContactEmail extends Generic\DAOEntity {
+
+}
